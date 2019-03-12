c79ff09caff58689367b60d450a043780d8d24cc
Kpu7k <45510391+kpu7k@users.noreply.github.com>
2019-03-12 23:57:16 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,41 @@
 (function () {
     'use strict';
-    let numbers = [1, 3, 5, 5, 8, 7, 7, 55, 55, 63, 28, 28];
-    
-   
-    function delRepeat (numbers) {
-        let unique = [];
+    let numbers = [1, 3, 5, 8, 7, 7, 55, 55, 63, 28, 28, 5];
+    let unique = [],
+        
+        repeat = [];
+
+    function delRepeat(numbers) {
+
         Input:
-        for(let i =0; i<numbers.length; i++){
+        for (let i = 0; i < numbers.length; i++) {
             let str = numbers[i];
-            for(let j = 0; j< unique.length; j++){
-                if(unique[j] == str) continue Input;
+            for (let j = 0; j < unique.length; j++) {
+                while (unique[j] == str) {
+                    repeat.push(str);
+                    continue Input;
+                }
             }
             unique.push(str);
         }
+        Input1:
+        for (let i = 0; i < repeat.length; i++) {
+            let str = repeat[i];
+            for (let j = 0; j < unique.length; j++) {
+                while (unique[j] == str) {
+                    unique.splice(j, 1);
+                    console.log(unique);
+                    continue Input1;
+                }
+            }
+        }
         return unique;
-     }
-     alert(delRepeat(numbers));
-  
+    }
+    function delUnique(repeat) {
+        
+        return unique;
+    }
+    alert(delRepeat(numbers));
+    
+
 }());
